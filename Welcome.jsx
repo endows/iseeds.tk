33c59de747e09b2256745ad7ed1dc84b901b0ccf
endows <you@example.com>
2015-08-04 05:00:46 +0900

    routing-2 post list

--- Welcome.jsx
+++ Welcome.jsx
@@ -1,8 +1,24 @@
 WelcomeComponent = React.createClass({
   render() {
     return <div>
-      <h1>Hello, {this.props.name}</h1>
+      <h1>私の名前は, {this.props.name}</h1>
+      <p>/my/name/is/〈あなたの名前〉を入力すると、名前が表示されます</p>
     </div>
   }
 });
-ReactLayout.render(WelcomeComponent, {name: "Arunoda"})
+
+FlowRouter.route("/", {
+  action: function() {
+    ReactLayout.render(MainLayout, {
+      content: <WelcomeComponent name={'takashi'} />
+    });
+  }
+});
+
+FlowRouter.route("/my/name/is/:name", {
+  action: function(params) {
+    ReactLayout.render(MainLayout, {
+      content: <WelcomeComponent name={params.name} />
+    });
+  }
+});
